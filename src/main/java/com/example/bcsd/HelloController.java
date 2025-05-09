1c2bb44ca9eb305761fdf60fe6eb9cc15749fdfc
hyunseong0307 <hslee000307@gmail.com>
2025-05-09 22:09:20 +0900

    자기소개 작성

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -2,19 +2,26 @@ package com.example.bcsd;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HelloController {
 
     @ResponseBody
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!!!!!";
+    @GetMapping("/introduce")
+    public String introduce(@RequestParam(name = "name", required = false, defaultValue = "홍길동") String name) {
+        return "안녕하세요 제 이름은 " + name + "입니다!";
     }
-
-    @GetMapping("/hello2")
-    public String hello2() {
-        return "hello";
+    @ResponseBody
+    @GetMapping("/json")
+    public Map<String, Object> json() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("age", 26);
+        map.put("name", "허준기");
+        return map;
     }
 }
