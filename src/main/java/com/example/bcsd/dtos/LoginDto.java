c2fce431b04f29d72f56ae30f4f432b700099bdb
hyunseong0307 <hslee000307@gmail.com>
2025-06-30 13:42:04 +0900

    session 인증방식 사용을 위한 DTO 생성

---
+++ src/main/java/com/example/bcsd/dtos/LoginDto.java
@@ -0,0 +1,11 @@
+package com.example.bcsd.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LoginDto {
+    private String account;
+    private String password;
+    private String confirmPassword;
+}
